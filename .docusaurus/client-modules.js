384fbfbbe55640851e86c9d456c2a0d08424ea9f
wenren <wrcj12138@163.com>
2025-07-16 23:32:48 +0800

    Add proper .gitignore rules and remove build artifacts from tracking

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("/Users/one/Documents/GitHub/wenren2025.github.io/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
-  require("/Users/one/Documents/GitHub/wenren2025.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/one/Documents/GitHub/wenren2025.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/one/Documents/GitHub/wenren2025.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/one/Documents/GitHub/wenren2025.github.io/src/css/custom.css"),
-];
